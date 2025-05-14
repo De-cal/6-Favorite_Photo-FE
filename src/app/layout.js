9c34ac5170cde71b5e2e848ab942165fefc5c9b1
Jam1eL1 <23jisu@gmail.com>
2025-05-14 17:59:51 +0900

    chore: 프론트엔드 프로젝트 기초 셋업

---
+++ src/app/layout.js
@@ -0,0 +1,35 @@
+import "./globals.css";
+const baskinRabins = localFont({
+  src: "../assets/fonts/br-font-bold.ttf",
+  weight: "100 900",
+  variable: "--font-baskinRabins",
+});
+
+const notoSans = localFont({
+  src: "../assets/fonts/noto-sans-kr.woff",
+  weight: "100 800",
+  variable: "--font-notoSans",
+});
+
+// const notoSansWoff2 = localFont({
+//   src: "../assets/fonts/noto-sans-kr.woff2",
+//   weight: "100 800",
+//   variable: "--font-notoSansWoff2",
+// });
+
+export const metadata = {
+  title: "최애의 포토",
+  description: "당신의 최애의 포토를 거래해보세요",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body
+        className={`${baskinRabins.variable} ${notoSans.variable} min-h-screen flex flex-col`}
+      >
+        <main className="relative flex-1 font-notoSans">{children}</main>
+      </body>
+    </html>
+  );
+}
