5fac8997365eca86915f013ff68bf7fc265bc5f0
Decal <vumublne@naver.com>
2025-05-17 22:19:14 +0900

    feat: Header 레이아웃 높이 수정(코드리뷰 반영)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,7 +6,7 @@ import logo from "@/assets/images/logo.svg";
 export default function Header() {
   return (
     <div className="flex justify-center items-center">
-      <div className="flex justify-between items-center w-full max-w-[1920px] h-[60px] px-[20px] sm:px-[40px] md:px-[220px]">
+      <div className="flex justify-between items-center w-full max-w-[1920px] h-[60px] px-[20px] sm:h-[70px] sm:px-[40px] md:h-[80px] md:px-[220px]">
         <div className="relative w-[22px] h-[22px] sm:hidden">
           <Image src={menu} alt="메뉴" fill className="object-cover" />
         </div>
