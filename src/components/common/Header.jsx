66d73d7eec149d688105ab597fb4f8afa588fa2e
Decal <vumublne@naver.com>
2025-05-18 18:42:57 +0900

    fix: Header 이미지 경로 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import menu from "@/assets/icons/menu.svg";
-import logo from "@/assets/images/logo.svg";
+import ic_menu from "@/assets/icons/ic-menu.svg";
+import img_logo from "@/assets/images/img-logo.svg";
 export default function Header() {
   return (
     <div className="flex justify-center items-center">
       <div className="flex justify-between items-center w-full max-w-[1920px] h-[60px] px-[20px] sm:h-[70px] sm:px-[40px] md:h-[80px] md:px-[220px]">
         <div className="relative w-[22px] h-[22px] sm:hidden">
-          <Image src={menu} alt="메뉴" fill className="object-cover" />
+          <Image src={ic_menu} alt="메뉴" fill className="object-cover" />
         </div>
         <Link
           href="/"
           className="absolute left-[50%] translate-x-[-50%] w-[83.37px] h-[15.12px] sm:static sm:left-auto sm:translate-x-0"
         >
-          <Image src={logo} alt="최애의 포토" fill className="object-cover" />
+          <Image
+            src={img_logo}
+            alt="최애의 포토"
+            fill
+            className="object-cover"
+          />
         </Link>
         <div className="flex justify-center items-center gap-[30px]">
           <Link
