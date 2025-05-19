c01f9913b5d0731f5dc197b34883aee3c1bd8e31
Jam1eL1 <23jisu@gmail.com>
2025-05-19 11:56:29 +0900

    feat: 공용 컴포넌트 액션버튼 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
+import ActionButton from "@/components/ui/buttons/ActionButton";
 import React from "react";
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <h1>공용 컴포넌트 한눈에 보기</h1>
+      <div className="bg-black flex flex-col gap-6">
+        <h1>Landing Page</h1>
+        <ActionButton variant="primary" label="포토카드 구매하기" />
+        <ActionButton variant="primary" label="포토카드 교환하기" />
+        <ActionButton variant="primary" label="포토카드 교환하기" disabled />
+        <ActionButton variant="primary" label="승인" />
+
+        <ActionButton variant="secondary" label="판매 내리기" />
+        <ActionButton variant="secondary" label="포토카드 교환하기" />
+        <ActionButton variant="secondary" label="거절" />
+      </div>
+    </>
+  );
 }
