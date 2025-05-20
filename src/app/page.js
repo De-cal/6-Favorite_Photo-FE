c62c31d0be81aaa04c2ced04838eb48ca1ff9f55
Jam1eL1 <23jisu@gmail.com>
2025-05-20 18:11:55 +0900

    style: 랜딩페이지 히어로와 피쳐1(포인트로 안전하게 거래하세요) 추가

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Link from "next/link";
 import React from "react";
 import ActionButton from "@/components/ui/buttons/ActionButton";
 import heroImg from "@/assets/images/img-landing-hero.svg";
-
+import heroBgImg from "@/assets/images/img-landing-hero-bg.svg";
+import logoImg from "@/assets/images/img-logo.svg";
+import featImg1 from "@/assets/images/img-landing-page-feature-1.svg";
 export default function LandingPage() {
   return (
     <>
@@ -66,41 +68,93 @@ export default function LandingPage() {
         </div>
       </div> */}
 
-      {/* 랜딩페이지 시작 */}
-      <div className="flex flex-col items-center justify-center">
+      {/* 랜딩페이지 전체 파트 */}
+      <div className="flex flex-col items-center justify-center w-full">
         {/* 랜딩 히어로*/}
-        <div className="">
-          <Image
-            src={heroImg}
-            alt="히어로 이미지"
-            fill
-            className="object-cover"
-          />
+        <div
+          className="flex flex-col items-center justify-center w-full
+          px-[16px] pt-[33px] pb-[12px] 
+          sm:px-[33px] sm:pt-[23px]
+          md:px-[61px] md:pt-[13px]
+          relative"
+        >
+          {/* 히어로 이미지 background */}
+          <div
+            className="relative flex items-center justify-center
+  w-full h-[412px] sm:h-[722px] md:h-[1088px]
+  rounded-2xl overflow-hidden z-0"
+          >
+            <Image
+              src={heroBgImg}
+              alt="히어로 백그라운드 이미지"
+              fill
+              className="object-cover"
+            />
+          </div>
+          {/* text wrapper */}
+          <div className="absolute top-[70px] left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center z-10">
+            <div className="relative overflow-hidden hidden sm:block sm:w-[138px] sm:h-[25px] sm:mb-[23px]">
+              <Image
+                src={logoImg}
+                alt="최애의 포토 로고"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <h1 className="text-white text-center text-[20px] leading-[26px] sm:leading-[48px] h-12 mb-6 font-bold sm:h-[96px] sm:text-[40px] sm:mb-[38px] md:mb-[33px]">
+              구하기 어려웠던
+              <br /> <span className="text-main">나의 최애</span>가 여기에!
+            </h1>
+            <Link href="/marketplace">
+              <ActionButton
+                variant="primary"
+                className="w-[150px] h-[40px] sm:w-[226px] sm:h-[55px] text-xs sm:text-base"
+              >
+                최애 찾으러 가기
+              </ActionButton>
+            </Link>
+          </div>
+
+          {/* 히어로 이미지 */}
+          <div
+            className="absolute top-[100px] sm:top-[180px] md:top-[250px] left-1/2 transform -translate-x-1/2 
+           w-full h-[457px] sm:h-[745px] md:h-[765px]
+           z-20"
+          >
+            <Image
+              src={heroImg}
+              alt="히어로 이미지"
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center">
-          {/* <Image src={} alt="" fill className="object-cover"/> */}
-          <h1 className="text-white">구하기 어려웠던 나의 최애가 여기에!</h1>
-          <ActionButton variant="primary" className="w-[150px] h-[40px]">
-            최애 찾으러 가기
-          </ActionButton>
-          <ActionButton variant="primary" className="w-[150px] h-[40px]">
-            포토카드 구매하기
-          </ActionButton>
+        {/* feat - 1 wrapper */}
+        <div className="relative  flex flex-col items-center justify-center w-full">
+          {/* feat - 1 text wrapper */}
+          <div className="flex flex-col items-start justify-center w-full pt-[67px] pl-[32px] sm:pl-[61px] gap-3 sm:gap-[14px]">
+            <h1 className="text-white font-bold text-[20px] sm:text-4xl ">
+              포인트로 <span className="text-main">안전하게 거래</span>하세요
+            </h1>
+            <p className="text-left text-sm sm:text-lg md:text-[#9f9f9f] font-normal">
+              내 포토카드를 포인트로 팔고, 원하는 포토카드를
+              <br />
+              포인트로 안전하게 교환하세요
+            </p>
+          </div>
+          {/* feat - 1 img */}
+          <div className="absolute top-[150px] left-[32px] sm:top-[200px] sm:left-[61px] md:top-[250px] overflow-hidden w-full h-[231px]">
+            <Image
+              src={featImg1}
+              alt="포인트로 안전하게 거래하세요"
+              fill
+              className="object-cover"
+            />
+          </div>
         </div>
-
-        {/* feature - 1 */}
-        <h1>포인트로 안전하게 거래하세요</h1>
       </div>
-      {/* <div className="relative w-[] h-[] sm:w-[] sm:h-[] md:w-[] md:h-[]">
-        <Image src={} alt="" fill className="object-cover" />
-      </div>
-      <div className="relative w-[] h-[] sm:w-[] sm:h-[] md:w-[] md:h-[]">
-        <Image src={} alt="" fill className="object-cover" />
-      </div>
-      <div className="relative w-[] h-[] sm:w-[] sm:h-[] md:w-[] md:h-[]">
-        <Image src={} alt="" fill className="object-cover" />
-      </div> */}
+      
     </>
   );
 }
