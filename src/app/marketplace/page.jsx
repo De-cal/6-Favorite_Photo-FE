7040928b633600608a91bdd5e804e5a9349194ae
sebiny <rucy8888@naver.com>
2025-05-21 15:48:56 +0900

    현재작업저장

--- src/app/marketplace/page.jsx
+++ src/app/marketplace/page.jsx
@@ -26,46 +26,66 @@ export default function MarketplacePage() {
           showFilter ? "pointer-events-none" : ""
         }`}
       >
-        <div className="px-[15px] sm:px-[20px] md:px-[220px] mt-[40px]">
-          <div className="hidden sm:flex sm:gap-[114px] md:gap-[720px]">
+        <div className="px-[15px] sm:px-[20px] md:px-[0px] mt-[40px]">
+          <div className="hidden sm:flex sm:gap-[114px] md:gap-[650px]">
             <Image
               src={marketplace}
               width={248}
               height={49}
               alt="marketplace"
+              className="md:hidden"
             />
-            <ActionButton variant="primary">포토카드 교환하기</ActionButton>
+            <Image
+              src={marketplace}
+              width={320}
+              height={63}
+              alt="marketplace"
+              className="hidden md:block"
+            />
+            <ActionButton className="w-[342px] h-[60px] md:w-[440px]">
+              포토카드 교환하기
+            </ActionButton>
           </div>
         </div>
-
-        <div className="w-full px-[15px] sm:px-[20px] md:px-[220px] mt-[15px] hidden sm:block">
-          <div className="w-full h-[1px] border-b border-gray-400 sm:border-gray-100 sm:h-[2px]" />
+        <div className="w-[704px] md:w-[1490px]">
+          <div className="w-full mt-[15px] hidden sm:block">
+            <div className="w-full h-[1px] border-b border-gray-400 sm:border-gray-100 sm:h-[2px]" />
+          </div>
         </div>
-        <div className="w-full">
-          <div className="flex mt-[20px] justify-center sm:justify-between sm:px-[20px] md:px-[220px] ">
-            <div className="flex items-center">
-              <Search />
-              <FilterDropdown />
+        <div className="w-[704px] md:w-[1480px]">
+          <div className="w-full">
+            <div className="flex mt-[20px] justify-center sm:justify-between md:justify-between">
+              <div className="flex items-center">
+                <Search />
+                <FilterDropdown />
+              </div>
+              <Sort className="hidden sm:flex md:flex" />
             </div>
-            <Sort className="hidden sm:flex md:flex" />
+          </div>
+        </div>
+        <div className="w-[347px]">
+          <div className="w-full px-[10px] sm:px-[20px] mt-[15px] block sm:hidden">
+            <div className="w-full h-[1px] border-b border-gray-400 sm:border-gray-100 sm:h-[2px]" />
           </div>
         </div>
 
-        <div className="w-full px-[15px] sm:px-[20px] mt-[15px] block sm:hidden">
-          <div className="w-full h-[1px] border-b border-gray-400 sm:border-gray-100 sm:h-[2px]" />
-        </div>
-
-        <div className="px-[15px] mb-[20px] flex justify-between w-full mt-[15px]">
-          <button
-            onClick={() => setShowFilter(true)}
-            className="sm:hidden sm:hidden cursor-pointer
+        <div className="w-[347px]">
+          <div className="w-full px-[10px] mb-[20px] flex justify-between w-full mt-[15px]">
+            <button
+              onClick={() => setShowFilter(true)}
+              className="sm:hidden sm:hidden cursor-pointer
 rounded-[2px] flex items-center justify-center border border-gray-200 w-[35px] h-[35px]"
-          >
-            <Image alt="filerIcon" src={filterIcon} width={20} height={20} />
-          </button>
-          <Sort className="flex sm:hidden md:hidden" />
+            >
+              <Image alt="filerIcon" src={filterIcon} width={20} height={20} />
+            </button>
+            <Sort className="flex sm:hidden md:hidden" />
+          </div>
         </div>
-        <div className="flex flex-wrap mt-[20px] gap-[5px] justify-center">
+        <div
+          className="grid grid-cols-2 sm:grid-cols-2 
+        md:grid-cols-3 gap-[5px] sm:gap-[20px] md:gap-[80px] mt-[20px]
+         justify-items-center"
+        >
           <Card />
           <Card />
           <Card />
