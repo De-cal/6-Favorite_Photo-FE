1308170ac00cd10e0ed579a0b3b13a0d9c019f40
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-05-23 13:21:09 +0900

    feat:모바일 필터링 구현중

--- src/app/my-gallery/_components/RankSection.jsx
+++ src/app/my-gallery/_components/RankSection.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import RankDiv from "./RankDiv";
 
 export default function RankSection({ data }) {
-  const totalAmount = data.count;
-  const common = data.common;
-  const rare = data.rare;
-  const superrare = data.superrare;
-  const legendary = data.legendary;
+  const totalAmount = data.length;
+  const common = data.filter((card) => card.photoCard.rank === "COMMON").length;
+  const rare = data.filter((card) => card.photoCard.rank === "RARE").length;
+  const superrare = data.filter(
+    (card) => card.photoCard.rank === "SUPER RARE"
+  ).length;
+  const legendary = data.filter(
+    (card) => card.photoCard.rank === "LEGENDARY"
+  ).length;
   return (
     <section className="flex flex-col gap-5 py-10 border-b border-gray-400 w-full md:max-w-[1480px]">
       <div className="flex flex-row gap-2.5">
         <span className="text-[14px]">
           {data.user
             ? `${data.user}님이 보유한 포토카드`
-            : "???님이 보유한 포토카드"}
+            : "유디 님이 보유한 포토카드"}
         </span>
         <span className="text-[14px] text-gray-300">
           {totalAmount ? `총 ${totalAmount}장` : "(총 ???장)"}
