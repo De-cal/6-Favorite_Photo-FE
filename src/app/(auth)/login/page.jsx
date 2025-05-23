c8658a2be695c13979a4a4afdc37557fc4bcc1f9
Jam1eL1 <23jisu@gmail.com>
2025-05-23 11:42:35 +0900

    feat: 로그인, 회원가입 폼 기초구조  생성, 페이지에 적용

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import LoginForm from "../_components/LoginForm";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <LoginForm></LoginForm>
+    </>
+  );
 }
