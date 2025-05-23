b5d5cd607276c1bdc530af4955286dbb3ad4b608
Jam1eL1 <23jisu@gmail.com>
2025-05-23 11:50:42 +0900

    feat: auth.js 기초 셋업, authValidators 유틸 함수 안에 로그인 회원가입 관련 인풋 validation 함수 생성

---
+++ src/api/auth.js
@@ -0,0 +1,5 @@
+const AUTH_API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function login({ email, password }) {}
+
+export async function signUp({ email, nickname, password, passwordConfirm }) {}
