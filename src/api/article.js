b9a55faf324ddf8970443a2d77a0cb6c4af9a00b
sebiny <rucy8888@naver.com>
2025-05-24 16:31:08 +0900

    chore: article.js 파일 수정

--- src/api/article.js
+++ src/api/article.js
@@ -1,7 +1,7 @@
 export async function getAllArticles(keyword) {
   try {
     const query = keyword ? `?keyword=${encodeURIComponent(keyword)}` : "";
-    const response = await fetch(`http://localhost:5050/articles${query}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/articles${query}`);
     const data = await response.json();
     console.log("data", data);
     return data;
