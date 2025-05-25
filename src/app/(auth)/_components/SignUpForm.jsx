f6db9c605f489f582ae590eebcb72f4ff30a9cf3
Jam1eL1 <23jisu@gmail.com>
2025-05-25 20:11:49 +0900

    useSignUpForm 훅 생성

--- src/app/(auth)/_components/SignUpForm.jsx
+++ src/app/(auth)/_components/SignUpForm.jsx
@@ -1,5 +1,38 @@
+"use client";
+import useRedirectIfAuthenticated from "@/hooks/useRedirectIfAuthenticated";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function SignUpForm() {
+  // 토큰이 있는 유저는 marketplace 페이지로 리다이렉트
+  useRedirectIfAuthenticated();
+  const {
+    email,
+    nickname,
+    password,
+    passwordConfirm,
+    isFormValid,
+    isEmailValid,
+    isNicknameValid,
+    isPasswordValid,
+    isPasswordConfirmValid,
+    isEmailTouched,
+    isNicknameTouched,
+    isPasswordTouched,
+    isPasswordConfirmTouched,
+    handleEmailChange,
+    handleNicknameChange,
+    handlePasswordChange,
+    handlePasswordConfirmChange,
+    resetForm,
+  } = useSignUpForm();
+
+  const router = useRouter;
+
+  // 임시 모달
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   return <div>SignUpForm</div>;
 }
--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import LoginForm from "../_components/LoginForm";
 import AuthLogo from "../_components/AuthLogo";
-import RedirectionLink from "../_components/RedirectionLink";
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center max-w-[520px] w-full h-full gap-[80px] px-[15px] mt-[168px] sm:mt-[316px] md:mt-[277px] mx-auto">
       <AuthLogo />
       <LoginForm />
-      
     </div>
   );
 }
