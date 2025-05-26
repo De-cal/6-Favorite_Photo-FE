7b0a466d7727fd05390e6a10df132a0f0874295c
Shinmilli <shinmilli100@gmail.com>
2025-05-26 15:48:21 +0900

    feat: img

--- src/api/card.js
+++ src/api/card.js
@@ -1,3 +1,4 @@
+// src/api/card.js
 export const getAllCards = async () => {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/cards`);
@@ -6,6 +7,26 @@ export const getAllCards = async () => {
     return data;
   } catch (error) {
     console.error("카드 목록을 가져오는데 실패했습니다:", error);
+    throw error;
+  }
+};
+
+export const createCard = async (formData) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/cards`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!res.ok) {
+      throw new Error("카드 생성에 실패했습니다.");
+    }
+
+    const data = await res.json();
+    console.log("카드 생성 성공:", data);
+    return data;
+  } catch (error) {
+    console.error("카드 생성 오류:", error);
     throw error;
   }
 };
