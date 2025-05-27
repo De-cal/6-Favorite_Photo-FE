966a234d7d3865aa55ea9c83e1ae699988371aa9
Jam1eL1 <23jisu@gmail.com>
2025-05-27 13:28:23 +0900

    feat: cookie fetch & default fetch 위한 fetchClient 라이브러리 생성

---
+++ src/lib/fetchClient.js
@@ -0,0 +1,37 @@
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5050";
+
+export async function cookieFetch(path, options = {}) {
+  const response = await fetch(`${API_BASE_URL}${path}`, {
+    credentials: "include", // 쿠키 보내기 위한 설정 (access/refresh tokens)
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "API 요청이 실패하였습니다");
+  }
+
+  return response.json();
+}
+
+export async function defaultFetch(path, options = {}) {
+  const response = await fetch(`${API_BASE_URL}${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "API request failed");
+  }
+
+  return response.json();
+}
