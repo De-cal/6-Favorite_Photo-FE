a207428e8338d3f85b6d62e6e59886fb4ab98dbc
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-05-27 14:39:13 +0900

    feat:마이갤러리 상세페이지 상상구현

---
+++ src/app/my-gallery/[id]/_components/CardInfo.jsx
@@ -0,0 +1,92 @@
+"use client";
+import Image from "next/image";
+import React, { useRef, useEffect, useState } from "react";
+import example from "@/assets/images/img-card-placeholder-1.svg";
+import { genreChange } from "@/utils/genreChange";
+import ActionButton from "@/components/ui/buttons/ActionButton";
+import { useModal } from "@/providers/ModalProvider";
+
+function CardInfo({ data }) {
+  const rankStyles = {
+    COMMON: "text-main",
+    RARE: "text-blue",
+    SUPERRARE: "text-purple",
+    LEGENDARY: "text-pink",
+  };
+
+  const infoRef = useRef(null);
+  const [infoHeight, setInfoHeight] = useState(0);
+
+  const { openModal, closeModal } = useModal();
+
+  useEffect(() => {
+    const updateHeight = () => {
+      if (window.innerWidth >= 640 && infoRef.current) {
+        setInfoHeight(infoRef.current.clientHeight);
+      } else {
+        setInfoHeight("auto"); // 모바일은 auto
+      }
+    };
+
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+
+  return (
+    <section className="mt-[26px] sm:mt-[48px] md:mt-[70px] flex flex-col sm:flex-row gap-5 w-full max-w-[1480px]">
+      <div
+        className="flex-1 min-w-[400px]"
+        style={{ height: infoHeight !== "auto" ? `${infoHeight}px` : "auto" }}
+      >
+        <Image
+          src={data.photoCard.imgURL === "" ? example : data.photoCard.imgURL}
+          alt={"사진이미지"}
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* ✅ 오른쪽 카드 정보 */}
+      <div className="w-full sm:max-w-[440px] flex flex-col" ref={infoRef}>
+        <div className="flex flex-row justify-between items-center w-full gap-4 pb-[34px] border-b-1 border-gray-400">
+          <div className="flex flex-row justify-between items-center">
+            <div
+              className={`pr-[10px] border-r-2 border-gray-400 ${
+                rankStyles[data.photoCard.rank] || ""
+              }`}
+            >
+              {data.photoCard.rank}
+            </div>
+            <div className="pl-[10px] text-gray-400">
+              {genreChange(data.photoCard.genre)}
+            </div>
+          </div>
+          <div className="text-sm">{data.photoCard.creator.nickname}</div>
+        </div>
+
+        <div className="py-[30px] border-b-1 border-gray-400">
+          {data.photoCard.description}
+        </div>
+        <div className="py-[30px] border-b-1 border-gray-400">
+          {`구매 가격 : ${data.price}P `}
+        </div>
+        <div className="py-[30px] border-b-1 border-gray-400 text-center">
+          시안 업데이트 시
+        </div>
+        <div className="py-[30px] border-b-1 border-gray-400 text-center">
+          다른 정보 추가 예정
+        </div>
+        <div className="py-[30px] border-b-1 border-gray-400 text-center">
+          <ActionButton
+            className="w-full h-20 text-2xl"
+            // onClick={()=>openModal(<판매하기 모달/>)}
+          >
+            판매하기
+          </ActionButton>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default CardInfo;
--- src/app/my-gallery/[id]/page.jsx
+++ src/app/my-gallery/[id]/page.jsx
@@ -1,5 +1,31 @@
+import MobileHeader from "@/components/common/MobileHeader";
 import React from "react";
+import TopTitle from "./_components/TopTitle";
+import CardInfo from "./_components/CardInfo";
 
 export default function MyGalleryDetailPage() {
-  return <div>MyGalleryDetailPage</div>;
+  const data = {
+    photoCard: {
+      title: "거리를 걷는 남자",
+      rank: "LEGENDARY",
+      description:
+        "흐릿한 오후, 거리를 유유자적하게 걷는 중년 남성의 아름다운 뒷태가 담긴 사진입니다.",
+      genre: "PORTRAIT",
+      imgURL: "",
+      creator: { nickname: "김숲안" },
+    },
+    price: 10,
+    quantity: 1,
+    status: "SELLING",
+    totalQuantity: 5,
+  };
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center max-w-[1480px] sm:px-5 mx-auto mt-[20px] px-5 gap-5">
+        <MobileHeader title="마이갤러리 상세 페이지" src="/my-gallery" />
+        <TopTitle data={data} />
+        <CardInfo data={data} />
+      </div>
+    </>
+  );
 }
