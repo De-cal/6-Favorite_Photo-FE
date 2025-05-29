419255e1462e4e3f67f6336abc705d2b33521d1d
rlawodnr1109 <wodnr1109@naver.com>
2025-05-29 15:05:26 +0900

    feat: 읽음 기능 구현.

--- src/components/modal/notification/NotificationsModal.jsx
+++ src/components/modal/notification/NotificationsModal.jsx
@@ -1,15 +1,25 @@
+import { readNotification } from "@/lib/api/notification.api";
 import NotificationCard from "./NotificationCard";
 
 function NotificationsModal({ notifications }) {
+  const handleReadNotification = (notificationId) => {
+    readNotification(notificationId);
+  };
+
   return (
     <div className="overflow-y-auto no-scrollbar h-[535px]">
       {notifications &&
         notifications.map((notification) => {
           return (
-            <NotificationCard
+            <div
               key={notification.id}
-              notification={notification}
-            />
+              onClick={() => handleReadNotification(notification.id)}
+            >
+              <NotificationCard
+                key={notification.id}
+                notification={notification}
+              />
+            </div>
           );
         })}
     </div>
