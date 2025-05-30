19b02485a726a8cb71954baa996ab1f2064fe1c8
rlawodnr1109 <wodnr1109@naver.com>
2025-05-30 10:14:42 +0900

    refactor: auth 관련 코드 추가.

--- src/components/modal/randomPoint/SurprisePointModal.jsx
+++ src/components/modal/randomPoint/SurprisePointModal.jsx
@@ -2,41 +2,29 @@
 
 import { useEffect } from "react";
 import RewardPoint from "./RewardPoint";
-// import { useAuth } from "@/providers/AuthProvider";
+import { useAuth } from "@/providers/AuthProvider";
 import { useModal } from "@/providers/ModalProvider";
 import { usePointTimer } from "@/providers/PointTimerProvider";
 import useInactiveTimer from "@/hooks/useInactiveTimer";
 
 function SurprisePointModal() {
-  // const { isLoggedIn } = useAuth();
+  const { user } = useAuth();
   const { hasOpportunity, formattedTime, resetTimer } = usePointTimer();
   const { openModal } = useModal();
 
   // 기회가 생겼을 때 자동으로 모달 열기
-  // useEffect(() => {
-  //   if (hasOpportunity && isLoggedIn) {
-  //     openModal(() => <RewardPoint />, "center", "center", true);
-  //   }
-  // }, [hasOpportunity, isLoggedIn]);
-
   useEffect(() => {
-    if (hasOpportunity) {
+    if (!!user && hasOpportunity) {
       openModal(() => <RewardPoint />, "center", "center", true);
     }
-  }, [hasOpportunity]);
+  }, [hasOpportunity, isLoggedIn]);
 
-  // TODO: 비활동 타이머 - 20분 비활동시 타이머 초기화
-  // const handleInactive = () => {
-  //   resetTimer();
-  //   console.log("비활동으로 인한 타이머 초기화");
-  // };
-  // useInactiveTimer(handleInactive, isLoggedIn);
-
+  // 비활동 타이머 - 20분 비활동시 타이머 초기화
   const handleInactive = () => {
     resetTimer();
     console.log("비활동으로 인한 타이머 초기화");
   };
-  useInactiveTimer(handleInactive, true);
+  useInactiveTimer(handleInactive, !!user);
 
   return (
     <div>
