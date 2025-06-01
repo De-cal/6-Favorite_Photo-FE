b19891acd9ce24ef1d39a5300aff53a7f0d4583d
choi-mk <xhrlmin10@naver.com>
2025-06-02 01:03:56 +0900

    fix: QA 1차 완성

--- src/app/marketplace/_components/marketplace/Dropdown.jsx
+++ src/app/marketplace/_components/marketplace/Dropdown.jsx
@@ -1,32 +1,45 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import down from "@/assets/icons/ic-down.svg";
 import Image from "next/image";
 
 function Dropdown({ type, value, onChange }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const dropdownRef = useRef(null);
   const GENRES = ["PORTRAIT", "LANDSCAPE", "ANIMAL", "OBJECT", "FOOD", "ETC"];
   const RANKS = ["COMMON", "RARE", "SUPER RARE", "LEGENDARY"];
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setIsModalOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleClick = (item) => {
     onChange(item);
     setIsModalOpen(false);
   };
   return (
-    <div className="w-full relative">
+    <div className="w-full relative cursor-pointer" ref={dropdownRef}>
       <div className="font-bold md:text-[20px]">{type}</div>
       <div
-        className="border-1 border-gray-200 flex py-4 px-5 mt-[10px] text-gray-200 justify-between"
+        className=" flex py-4 px-5 mt-[10px] text-gray-200 justify-between border-1 border-gray-200"
         onClick={() => setIsModalOpen((prev) => !prev)}
       >
         {value}
         <Image alt="down" src={down} className="w-5 h-5 " />
       </div>
       {isModalOpen && (
-        <div className="absolute z-10 w-full">
+        <div className="absolute z-10 w-full border-1 border-gray-200 mt-[5px]">
           {(type === "등급" ? RANKS : GENRES).map((item) => (
             <div
-              className="border-1 border-gray-200 py-4 pl-5 bg-black w-full"
+              className=" py-4 pl-5 bg-black w-full"
               onClick={() => handleClick(item)}
               key={item}
             >
