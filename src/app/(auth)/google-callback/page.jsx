23c38e70e3a58116971e811d6743e9dfebb2dd8a
Jam1eL1 <23jisu@gmail.com>
2025-06-02 18:42:22 +0900

    refactor: handleAuthCallback 추가하여 getUser 이 반복적으로 me 엔드포인트로 api 요청 보내는거 방지

--- src/app/(auth)/google-callback/page.jsx
+++ src/app/(auth)/google-callback/page.jsx
@@ -1,23 +1,38 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useAuth } from "@/providers/AuthProvider"; 
+import { useAuth } from "@/providers/AuthProvider";
+import Loading from "@/components/common/Loading";
 
 export default function GoogleCallbackHandlerPage() {
   const router = useRouter();
-  const { getUser } = useAuth(); // 구글 인증 성공후 유저의 상태 갱신
+  const { getUser } = useAuth();
+  const [hasProcessed, setHasProcessed] = useState(false);
 
   useEffect(() => {
-    getUser(); 
+    // 이미 처리가 완료되었다면, 더 이상 실행하지 않고 즉시 종료
+    if (hasProcessed) {
+      return;
+    }
 
-    // 원하는 목적지 페이지로 리다이렉트
-    router.push("/marketplace");
-  }, [router, getUser]); 
+    const handleAuthCallback = async () => {
+      try {
+        await getUser();
+        setHasProcessed(true); // <--- 플래그를 true로 설정하여 다음 리렌더링 시 getUser 실행 방지
+        router.push("/marketplace");
+      } catch (error) {
+        console.error(
+          "Google Callback: 구글에서 유저정보를 가져오는데 실패하였습니다:",
+          error,
+        );
+      }
+    };
+    handleAuthCallback();
+  }, [router, hasProcessed]);
 
   return (
     <div>
-      <p>Google 로그인 처리 중...</p>
-      {/* 로딩 스피너 등을 여기에 표시할 수 있습니다. */}
+      <Loading />
     </div>
   );
 }
