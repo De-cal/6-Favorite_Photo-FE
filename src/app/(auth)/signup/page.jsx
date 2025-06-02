14fb2307b0b4a06907e91bab1d08bafa43b7c4e5
Jam1eL1 <23jisu@gmail.com>
2025-06-03 00:28:09 +0900

    style: signup& login 페이지 감싸는 div 마진 조정

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -4,7 +4,7 @@ import AuthLogo from "../_components/AuthLogo";
 
 export default function SignupPage() {
   return (
-    <div className="flex flex-col items-center justify-center max-w-[520px] w-full h-full gap-[60px] px-[15px] mt-[80px] mb-[58px] sm:mb-[130px] md:mb-[76px] sm:mt-[316px] md:mt-[330px] mx-auto">
+    <div className="flex flex-col items-center justify-center max-w-[520px] w-full h-full gap-[60px] px-[15px] mt-[80px] mb-[58px] sm:mt-[100px] md:mt-[110px] mx-auto">
       <AuthLogo />
       <SignUpForm />
     </div>
