a05ed82b8b49cddb68ea02172c67cae3fe555329
Decal <vumublne@naver.com>
2025-06-04 14:09:51 +0900

    chore: next.config.js 수정

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,9 @@ const nextConfig = {
       {
         hostname: "localhost",
         port: "5050",
-        pathname: "/uploads/**",
       },
       {
         hostname: "six-favorite-photo-team1-server.onrender.com",
-        pathname: "/uploads/**",
       },
     ],
   },
