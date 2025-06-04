39fe6152f6c4f748df508b1ed2b186aa76f77e2b
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-06-04 18:35:02 +0900

    fix:문구수정;

--- src/app/my-sell/_components/RankSection.jsx
+++ src/app/my-sell/_components/RankSection.jsx
@@ -5,9 +5,7 @@ export default function RankSection({ totalCount, rankCounts, user }) {
   return (
     <section className="flex flex-col gap-5 py-10 border-b border-gray-400 w-full md:max-w-[1480px]">
       <div className="flex flex-row gap-2.5">
-        <span className="text-[14px]">
-          {user ? `${user}님이 보유한 포토카드` : "유디 님이 보유한 포토카드"}
-        </span>
+        <span className="text-[14px]">{`${user}님이 판매중인 포토카드`}</span>
         <span className="text-[14px] text-gray-300">
           {`총 ${totalCount}장`}
         </span>
